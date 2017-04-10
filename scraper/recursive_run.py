43440e1fbe3a526e4804f7e18e0dfbcd72b6a50c
Karthick Perumal <Karamya@users.noreply.github.com>
2017-04-10 17:43:30 +0200

    Add files via upload

---
+++ scraper/recursive_run.py
@@ -0,0 +1,11 @@
+# @Author: Karthick <ramya>
+# @Date:   2017-03-31T21:50:57+02:00
+# @Last modified by:   ramya
+# @Last modified time: 2017-03-31T22:50:00+02:00
+
+
+
+import os
+
+while True:
+    os.system("python scrape_subreddit.py")
